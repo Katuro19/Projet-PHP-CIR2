1583774abe1889fc829f2ac2ff28f2fa5b1a4185
Katuro19 <151616494+Katuro19@users.noreply.github.com>
2024-12-12 17:35:49 +0100

    some inserts

--- database/Inserts.sql
+++ database/Inserts.sql
@@ -1,6 +1,15 @@
 -- Don't forget to use this after creating the database or you'll cry later on
 
 
-INSERT INTO "expertise" ("name") VALUES ('Cardiology'), ('Pediatrics'), ('Orthopedics'), ('Dermatology'), ('Neurology');
+BEGIN;
 
-INSERT INTO "locations" ("name","postcode") values ('GreatCity','12500'), ('DownTown','21600'), ('AquaCity','80800');
+INSERT INTO "expertise" VALUES (0,'Cardiology'), (1,'Pediatrics'), (2,'Orthopedics'), (3,'Dermatology'), (4,'Neurology'), (5,'Psychology');
+
+INSERT INTO "locations" values (0,'GreatCity','12500'), (1,'DownTown','21600'), (2,'AquaCity','80800'), (3,'Senatus populusque Romanus','50927');
+
+INSERT INTO "patients" values (0,'Kévin','Pereira','thisIsMyRealEmail@gmail.com','0771749163','cryptedPassword');
+INSERT INTO "patients" values (1,'Louis','Lacoste','anotherRealEmail@gmail.com','0620239690','password');
+
+INSERT INTO "doctors" values (0,'Oscar','Lavigne','yesyes@outlook.com','0649777963','verydemurepassword','78500',5);
+
+INSERT INTO "rendezvous" values (0,'01/25/0470','08:00','09:30',0 ,0, 3);
