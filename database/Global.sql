af1a5901bbee0b43ba7ceadc6084dfef7365fc55
Katuro19 <151616494+Katuro19@users.noreply.github.com>
2024-12-12 14:03:41 +0100

    File Global.sql

--- database/Global.sql
+++ database/Global.sql
@@ -1,4 +1,26 @@
 BEGIN TRANSACTION;
+
+CREATE TABLE IF NOT EXISTS "expertise" (
+	"id" SERIAL PRIMARY KEY NOT NULL UNIQUE,
+	"name"	TEXT NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS "locations" (
+	"id" SERIAL PRIMARY KEY NOT NULL UNIQUE,
+	"name" TEXT NOT NULL,
+	"postcode"	TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "patients" (
+	"id" SERIAL PRIMARY KEY NOT NULL UNIQUE,
+	"firstname" TEXT NOT NULL,
+	"lastname"	TEXT NOT NULL,
+	"email" TEXT NOT NULL,
+	"phone" TEXT NOT NULL,
+	"password" TEXT NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS "doctors" (
 	"id" SERIAL PRIMARY KEY NOT NULL UNIQUE,
 	"firstname"	TEXT NOT NULL,
@@ -7,29 +29,27 @@ CREATE TABLE IF NOT EXISTS "doctors" (
 	"phone" TEXT NOT NULL,
 	"password"	TEXT NOT NULL DEFAULT 'password',
 	"postcode" TEXT NOT NULL,
-	"expertise" TEXT NOT NULL
-	--PRIMARY KEY("id" ,SERIAL)
+	"expertise_id" INTEGER NOT NULL,
+	FOREIGN KEY("expertise_id") REFERENCES "expertise"("id") ON DELETE CASCADE
+
 );
 
 
-CREATE TABLE IF NOT EXISTS "patients" (
+CREATE TABLE IF NOT EXISTS "rendezvous" (
 	"id" SERIAL PRIMARY KEY NOT NULL UNIQUE,
-	"lastname"	TEXT NOT NULL,
-	"firstname"	TEXT NOT NULL,
-	"phone"	TEXT,
-	"birth_date"	TEXT NOT NULL DEFAULT 01012000,
-	"doctor_id" INTEGER,
+	"date" TEXT NOT NULL,
+	"start"	TEXT NOT NULL,
+	"end" TEXT NOT NULL,
+	"patient_id" INTEGER,
+	"doctor_id" INTEGER NOT NULL,
+	"location_id" INTEGER NOT NULL,
+	FOREIGN KEY("patient_id") REFERENCES "patients"("id") ON DELETE CASCADE,
+	FOREIGN KEY("location_id") REFERENCES "locations"("id") ON DELETE CASCADE,
 	FOREIGN KEY("doctor_id") REFERENCES "doctors"("id") ON DELETE CASCADE
-	-- PRIMARY KEY("id" SERIAL)
-);
-CREATE TABLE IF NOT EXISTS "glucose" (
-	"id"	SERIAL PRIMARY KEY  NOT NULL UNIQUE,
-	"patient_id" INTEGER NOT NULL,
-	"value"	TEXT,
-	"measurement_date"	TEXT,
-	FOREIGN KEY("patient_id") REFERENCES "patients"("id") ON DELETE CASCADE
-	-- PRIMARY KEY("id" SERIAL)
+
 );
 
-COMMIT;
+-- DONT FORGET TO CHECK AND COMMIT !!
+
+
 
