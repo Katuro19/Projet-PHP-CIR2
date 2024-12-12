37aa0fb2f6196d926f4f8556002fdd853ba40f0c
Katuro19 <151616494+Katuro19@users.noreply.github.com>
2024-12-12 16:21:54 +0100

    insert.php i mean what you waiting for

---
+++ database/Inserts.sql
@@ -0,0 +1,6 @@
+-- Don't forget to use this after creating the database or you'll cry later on
+
+
+INSERT INTO "expertise" ("name") VALUES ('Cardiology'), ('Pediatrics'), ('Orthopedics'), ('Dermatology'), ('Neurology');
+
+INSERT INTO "locations" ("name","postcode") values ('GreatCity','12500'), ('DownTown','21600'), ('AquaCity','80800');
