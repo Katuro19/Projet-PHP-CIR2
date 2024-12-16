e4a1a77ecfad4465e08870e32a3e9ecf3b200b55
Katuro19 <151616494+Katuro19@users.noreply.github.com>
2024-12-16 10:21:42 +0100

    patch login again

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ session_start();
 // Check if the user is already logged in via session
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
     // Redirect to dashboard if already logged in
-    if($_SESSION['user_type' === 'doctor'])
+    if($_SESSION['user_type'] === 'doctor')
         header("Location: doctors_home.php");
     else
         header("Location: patient_home.php");
