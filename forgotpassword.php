c4d3c0a3914bb096c670d498bdf8088a2c918caa
komiko44240BB <147724844+Pierre-LucMAURICE@users.noreply.github.com>
2025-01-05 20:20:49 +0100

    added forgot password page

---
+++ forgotpassword.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Reset Confirmation</title>
+    <link rel="stylesheet" href="forgotpassword_style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <h2>Reset Link Sent</h2>
+            <p>
+                A reset link has been sent to your email. Please check your inbox
+                and follow the instructions to reset your password.
+            </p>
+            <button onclick="window.location.href='login.php'" class="button">Back to Sign In</button>
+        </div>
+    </div>
+</body>
+</html>
