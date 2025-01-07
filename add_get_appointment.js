92975358a8005e28c42e98a11b7000b8c3f9f4f9
komiko44240BB <147724844+Pierre-LucMAURICE@users.noreply.github.com>
2025-01-07 17:13:14 +0100

    Update add_get_appointment.js

--- add_get_appointment.js
+++ add_get_appointment.js
@@ -1,10 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
     if (!is_doctor()) {
         filter();
+        filter_doctors();
         document.getElementById("selected_available_doctor").addEventListener("change", filter);
         document.getElementById("selected_available_location").addEventListener("change", filter);
+        document.getElementById("selected_available_expertise").addEventListener("change", filter_doctors);
     }
 });
+
+function filter_doctors(){
+    let inputed_expertise = document.getElementById("selected_available_expertise").value;
+    if(inputed_expertise != "default"){
+        inputed_expertise = inputed_expertise.split("_")[1];
+    }
+
+    let available_doctors = document.querySelectorAll('[value^="doctor_"]');
+
+    available_doctors.forEach(doctor => {
+        let associated_expertise = doctor.value.split("_")[2];
+         if(inputed_expertise == associated_expertise){
+            doctor.removeAttribute("style");
+         } else {
+            doctor.style.display = "none";
+         }
+    });
+}
+
 
 function filter() {
     let inputed_doctor = document.getElementById("selected_available_doctor").value;
@@ -26,9 +47,9 @@ function filter() {
         let doctor_id = classList.find(cls => cls.startsWith("doctor_"))?.split("_")[1];
         let location_id = classList.find(cls => cls.startsWith("location_"))?.split("_")[1];
 
-        if (inputed_location === "default" || inputed_location === "default") {
-            slot.style.display = "none";
-        }
+        // if (inputed_location === "default" || inputed_location === "default") {
+        //     slot.style.display = "none";
+        // }
         if (doctor_id === inputed_doctor && location_id === inputed_location) {
             slot.removeAttribute("style");
         } else {
