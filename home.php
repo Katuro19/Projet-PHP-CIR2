02ab43849a0bf032f3328acf94afad34c401627b
komiko44240BB <147724844+Pierre-LucMAURICE@users.noreply.github.com>
2025-01-08 20:29:19 +0100

    Update home.php

--- home.php
+++ home.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <?php
 session_start();
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
 include_once 'database/requests.php';
 include_once 'database/databases.php';
 // Check if the user is already logged in
